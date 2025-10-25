010e4f5cc1f478bae427d04212dbba86f8d6c4e4
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-10-25 22:43:44 +0300

    start and pause buttons

---
+++ prodigy-infotech/internship-2.0/web-dev-internship/task-2-stopwatch-web-application/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".startBtn").onclick = function() {
+    console.log("Start button pressed!");
+};
+
+document.querySelector(".pauseBtn").onclick = function() {
+    console.log("Pause button pressed!");
+};
+
