77fdee6b51bf86928cd55aca36d839880cc5758b
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-11-05 15:15:44 +0300

    add finalvalueafteroperations.py

---
+++ leetcode/final-value-after-operations.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def finalValueAfterOperations(self, operations: list[str]) -> int:
+        X=0
+        for op in operations:
+            if op == "++X" or op == "X++":
+                X += 1
+            elif op == "--X" or op == "X--":
+                X -= 1
+        return X
