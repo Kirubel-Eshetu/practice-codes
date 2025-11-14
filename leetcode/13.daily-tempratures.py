1995d00223240838f07b8ae1336d1dbcf1a37c80
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-11-14 10:03:19 +0300

    solution 13-end 14-start

---
+++ leetcode/13.daily-tempratures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTempratures(self, tempratures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                answer[prev_day] = i - prev_day
+            stack.append(i)
+        return answer
+
+
+    
