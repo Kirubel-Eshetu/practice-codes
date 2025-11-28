9c0609b0e55bc32216f7625d32dd2d09c340929c
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-11-28 15:14:02 +0300

    add of problem number of students unable to eat lunch

---
+++ leetcode/33.number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count0 = students.count(0)
+        count1 = students.count(1)
+
+        for s in sandwiches:
+            if s == 0:
+                if count0 > 0:
+                    count0 -= 1
+                else:
+                    break
+            else:
+                if count1 > 0:
+                    count1 -= 1
+                else:
+                    break
+        return count0 + count1
