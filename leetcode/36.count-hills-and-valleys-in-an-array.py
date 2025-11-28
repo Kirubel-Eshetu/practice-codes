3747206d3b24cf66576bff95b5918bd0cae7e173
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-11-28 15:53:30 +0300

    prob number 36 it is

---
+++ leetcode/36.count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        arr = [nums[0]]
+        for n in nums[1:]:
+            if n != arr[-1]:
+                arr.append(n)
+
+        count = 0
+        for i in range(1, len(arr) - 1):
+            if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+                count += 1
+            elif arr[i] < arr[i - 1] and arr[i] < arr[i + 1]:
+                count += 1
+            
+        return count
