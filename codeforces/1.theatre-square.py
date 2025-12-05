da3d2bd0da14128f89e6edec089612597304b6e9
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-12-05 11:00:49 +0300

    theatre-square done with pleasure

--- codeforces/1.theatre-square.py
+++ codeforces/1.theatre-square.py
@@ -1,1 +1,9 @@
-from typing import List
+from math import ceil
+
+n, m, a = map(int, input().split())
+
+flagAlongHeight = ceil(n / a)
+flagAlongWidth = ceil(m /a)
+
+totalFlag = flagAlongHeight * flagAlongWidth
+print(totalFlag)
