fabd785c3da4e70b49ae8a671713ccd9fe2b86b4
Krypton <kirubelwinner@gmail.com>
2026-04-21 14:10:45 +0300

    to be deployed

--- prodigy-infotech/web-dev-internship/task-1-responsive-landing-page/script.js
+++ prodigy-infotech/web-dev-internship/task-1-responsive-landing-page/script.js
@@ -1,16 +1,21 @@
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 1650) {
-        navbar.style.backgroundColor = '#000000';
+        navbar.style.backgroundColor = "#2b2b2b";
     }
     else if (window.scrollY > 1100) {
-        navbar.style.backgroundColor = '#001a9f';
+        navbar.style.backgroundColor = "#1a30a0";
     }
     else if (window.scrollY > 550) {
-        navbar.style.backgroundColor = '#002aff';
+        navbar.style.backgroundColor = "#1b41ff";
     }
     else {
-        navbar.style.backgroundColor = '#4060fd';
+        navbar.style.backgroundColor = "#4c6afe";
     }
 });
+
+const newYear = new Date().getFullYear();
+const yearUpdate = document.getElementById("footerYear");
+
+yearUpdate.textContent = newYear;
